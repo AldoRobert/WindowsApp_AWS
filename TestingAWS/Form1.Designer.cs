8e89a89d1e426036cf7245a826163eb75b477869
Aldo Arias <aldorobert2005@yahoo.es>
2025-08-25 11:55:21 -0400

    TabControl added

--- TestingAWS/Form1.Designer.cs
+++ TestingAWS/Form1.Designer.cs
@@ -28,18 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            btnSendSNS = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(0, 45);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(952, 458);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnSendSNS);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(944, 430);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "SNS";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(944, 430);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnSendSNS
+            // 
+            btnSendSNS.Location = new Point(30, 41);
+            btnSendSNS.Name = "btnSendSNS";
+            btnSendSNS.Size = new Size(56, 22);
+            btnSendSNS.TabIndex = 0;
+            btnSendSNS.Text = "Send";
+            btnSendSNS.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(954, 505);
+            Controls.Add(tabControl1);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button btnSendSNS;
+        private TabPage tabPage2;
     }
 }
